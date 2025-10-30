8986bb28fa7b3ca1d34c61fc873f324db6707ade
Abhishek Hosamani <abhishekhosamani522@gmail.com>
2025-10-30 23:38:59 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -190,6 +190,6 @@ if __name__ == '__main__':
     print("=" * 80 + "\n")
     
    # app.run(debug=True, host='0.0.0.0', port=5000)
-        import os
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
